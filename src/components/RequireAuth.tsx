82cf7565bfeda4abfad8d083d7b8f91cf709bba6
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2023-06-06 09:31:33 +0200

    feat: Login
    Add login page to provide API token or to mock API requests

---
+++ src/components/RequireAuth.tsx
@@ -0,0 +1,23 @@
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import useAuth from '../hooks/auth';
+
+function RequireAuth(): JSX.Element {
+  const { user } = useAuth();
+  const location = useLocation();
+
+  if (!user) {
+    return (
+      <Navigate
+        to="/login"
+        state={{
+          from: location,
+        }}
+        replace
+      />
+    );
+  }
+
+  return <Outlet />;
+}
+
+export default RequireAuth;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,5 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_OPENAPI_API_KEY: string;
-}
-
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
